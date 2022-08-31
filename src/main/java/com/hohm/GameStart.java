fb2d1b8c842720d57629be6be0261ed73bfc7b1a
Katy Fernelius <kkin001@gmail.com>
2022-08-31 12:31:57 -0500

    Created GameRunner class and ability to start a new game, AB#15

--- src/main/java/com/hohm/GameStart.java
+++ src/main/java/com/hohm/GameStart.java
@@ -1,8 +1,16 @@
 package main.java.com.hohm;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Locale;
+
+
 public class GameStart {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
+        GameRunner.gameInit();
 
     }
 }
