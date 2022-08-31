684e58a85f97d87112dfaccc6aeb2f5f673dc6d4
Katy Fernelius <kkin001@gmail.com>
2022-08-31 15:38:16 -0500

    Created Json class to parse json files, also established code as maven project

--- src/main/java/com/hohm/GameStart.java
+++ src/main/java/com/hohm/GameStart.java
@@ -1,16 +1,27 @@
-package main.java.com.hohm;
+package com.hohm;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Locale;
+import java.util.Map;
 
 
 public class GameStart {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
 
         GameRunner.gameInit();
 
+        //Code below is to parse the JSON file and return single string value
+//        File file = new File("src/main/resources/utils.json");
+//        JsonNode node = Json.parse(file);
+//        System.out.println(node.get("intro").textValue());
     }
 }
