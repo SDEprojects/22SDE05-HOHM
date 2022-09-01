fb59f51137e65d077eea31b86b13e1cf9c564075
Katy Fernelius <kkin001@gmail.com>
2022-09-01 14:56:11 -0500

    Added ability to parse JSON file and create rooms in the game to interact with, AB#25

--- src/main/java/com/hohm/Json.java
+++ src/main/java/com/hohm/Json.java
@@ -3,9 +3,15 @@ package com.hohm;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.hohm.models.MemeRoom;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Json {
 
@@ -21,7 +27,21 @@ public class Json {
     }
 
     //allows you to parse a JSON file and create an object from the information in the JSON file
-    public static <A> A fromJson(JsonNode node, Class<A> clazz) throws JsonProcessingException {
-        return objectMapper.treeToValue(node, clazz);
+    public static Map<String, MemeRoom> generateRooms() throws IOException {
+        //String pathName = String.format("src/main/resources/%s.json",roomName);
+        String[] rooms = {"hallway", "kitchen", "livingroom", "diningroom", "basement" };
+
+        //Reading the rooms.json file and generating a list of rooms
+        byte[] jsonData = Files.readAllBytes(Paths.get("src/main/resources/rooms.json"));
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode rootNode = objectMapper.readTree(jsonData);
+        Map<String, MemeRoom> roomMap = new HashMap<>();
+
+        //Populating the arraylist with the room objects
+        for (int i = 0; i< rooms.length;i++) {
+            JsonNode roomNode = rootNode.path(rooms[i]);
+            roomMap.put(rooms[i], objectMapper.convertValue(roomNode, MemeRoom.class));
+        }
+        return roomMap;
     }
 }
